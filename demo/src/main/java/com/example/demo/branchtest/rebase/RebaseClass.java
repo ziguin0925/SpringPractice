fbc5049be11ef8e2e269147856f1b75b71d34e48
RG <jlwoo092513@gmail.com>
2024-11-09 18:37:04 +0900

    Feat:Create Method in RebaseClass

--- demo/src/main/java/com/example/demo/branchtest/rebase/RebaseClass.java
+++ demo/src/main/java/com/example/demo/branchtest/rebase/RebaseClass.java
@@ -1,4 +1,12 @@
 package com.example.demo.branchtest.rebase;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class RebaseClass {
+    public String createMethod(){
+        return "Create Method;";
+    }
 }
