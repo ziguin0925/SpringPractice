bdfe341fecd27f2148a5827eeae45bddab1364fd
RG <jlwoo092513@gmail.com>
2024-11-09 17:25:24 +0900

    :art: git branch test
    test

--- demo/src/test/java/com/example/demo/JPA/Repository/User/UserRepositoryTest.java
+++ demo/src/test/java/com/example/demo/JPA/Repository/User/UserRepositoryTest.java
@@ -3,7 +3,9 @@ package com.example.demo.JPA.Repository.User;
 import com.example.demo.Entity.user.Role;
 import com.example.demo.Entity.user.User;
 import com.example.demo.Entity.user.UserState;
+import com.example.demo.Entity.user.dto.ATestDto;
 import com.example.demo.Entity.user.dto.UserProfileDto;
+import jakarta.transaction.TransactionScoped;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -90,5 +92,18 @@ class UserRepositoryTest {
         System.out.println(encodedPassword);
     }
 
+    @Test
+    @Transactional
+    @DisplayName("branchTest")
+    public void testBranch(){
+        User user = createOneUser();
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("TestDtoCreate")
+    public void testDtoCreate(){
+        ATestDto testDto = new ATestDto();
+    }
 
 }
